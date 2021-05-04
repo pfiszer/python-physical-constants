2083e98b9ed4e355f5d86944e8daa69bccabfc25
Pawek <44177078+PawekPL@users.noreply.github.com>
2021-05-04 23:30:44 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='physical-const',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Physical constants library',
+    author='Paweł "Pawek" Fiszer',
+    license='MIT',
+)
